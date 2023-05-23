f6474d140383c12c11c9ee3ca24d4d707722492c
Miguel Catalina-Gallego <miguel.catalina@gmail.com>
2023-05-23 06:36:47 -0400

    Add map rendering.

--- Application.cpp
+++ Application.cpp
@@ -1,7 +1,8 @@
 #include "Application.hpp"
 
 FredApp::FredApp()
-    : w_and_r(sdl::createWindowAndRenderer(5 * 256, 5 * 192)), tmgr(getRenderer())
+//    : w_and_r(sdl::createWindowAndRenderer(5 * 256, 5 * 192)), tmgr(getRenderer())
+    : w_and_r(sdl::createWindowAndRenderer(33*32, 33*40)), tmgr(getRenderer())
 {
-    SDL_RenderSetScale(getRenderer(), 5, 5);
+    //SDL_RenderSetScale(getRenderer(), 5, 5);
 }
--- GameMap.hpp
+++ GameMap.hpp
@@ -5,10 +5,16 @@
 #include <vector>
 #include <random>
 
+struct SDL_Rect;
+struct SDL_Renderer;
+struct SDL_Texture;
+class TextureManager;
 
 class GameMap
 {
 public:
+    static constexpr int cell_width = 32;
+    static constexpr int cell_height = 40;
     enum class Cell : std::uint8_t
     {
         empty,
@@ -23,18 +29,22 @@ public:
         trapdoor
     };
 
-    struct CellPos {
+    struct CellPos
+    {
         int x = 0, y = 0;
-        CellPos hmove(int delta) const { return { x + delta, y }; }
-        CellPos vmove(int delta) const { return { x, y + delta }; }
+        CellPos hmove(int delta) const { return {x + delta, y}; }
+        CellPos vmove(int delta) const { return {x, y + delta}; }
     };
     GameMap(std::minstd_rand &random_engine, int size_x, int size_y);
-    Cell getCell(CellPos const& pos);
+    Cell getCell(CellPos const &pos) const;
     int getWidth() const { return width; }
     int getHeight() const { return height; }
+    void render(int x, int y, TextureManager const &tmgr,
+                SDL_Renderer *renderer, SDL_Rect const *dest);
 
 private:
     int width, height;
     std::vector<Cell> contents;
-    void setCell(CellPos const& pos, Cell c);
+    void setCell(CellPos const &pos, Cell c);
+    SDL_Texture *getTextureOf(TextureManager const& tmgr, CellPos const &pos) const;
 };
