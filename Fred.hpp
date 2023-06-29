7ee48ca9faf8d81221caf0a49a4f762653b550ae
Miguel Catalina-Gallego <miguel.catalina@gmail.com>
2023-06-29 09:17:28 -0400

    Support for standing, walking, jumping.
    Shooting and sounds still missing.

--- Fred.hpp
+++ Fred.hpp
@@ -14,6 +14,17 @@ protected:
     std::pair<TextureID, CenterPos> getTexture() const override;
 
 private:
+    enum class State
+    {
+        REST_ON_FOOT,
+        WALK,
+        VERTICAL_JUMP,
+        SIDE_JUMP,
+        REST_ON_THE_ROPE,
+        ROPE_CLIMB,
+        FIRE,
+        EXIT_MAZE,
+    };
     enum class Direction : std::uint8_t
     {
         LEFT = 0,
@@ -34,9 +45,18 @@ private:
     CellPos nextCellPos() const {
         return sSpritePos.cellPos().hmove(2 * static_cast<int>(direction) - 1);
     }
+    CellPos prevCellPos() const {
+        return sSpritePos.cellPos().hmove(1 - 2 * static_cast<int>(direction));
+    }
     std::pair<int, int> updateRestOnFoot(GameMap const& game_map, unsigned action);
-    std::pair<int, int> updateWalking(GameMap const& game_map, unsigned action);
+    std::pair<int, int> updateWalk(GameMap const& game_map, unsigned action);
+    std::pair<int, int> updateVerticalJump(GameMap const &game_map, unsigned action);
+    std::pair<int, int> updateSideJump(GameMap const &game_map, unsigned action);
+    std::pair<int, int> updateRestOnTheRope(GameMap const &game_map, unsigned action);
+    std::pair<int, int> updateRopeClimb(GameMap const &game_map, unsigned action);
 
     Direction direction = Direction::LEFT;
     Type type = Type::STANDING;
+    int sStage = 0;
+    State sState = State::REST_ON_FOOT;
 };
