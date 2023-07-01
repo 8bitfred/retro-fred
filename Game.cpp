269cb2c4dd87d23d24eb30ffb1af38a37d3416a0
Miguel Catalina-Gallego <miguel.catalina@gmail.com>
2023-07-01 12:12:12 -0400

    Refactor Game structure.
    Use Game structure to keep the state of the game, and pass it to the
    fred state processing functions so that each individual state can
    examine the global state and update it.

--- Game.cpp
+++ Game.cpp
@@ -2,6 +2,25 @@
 #include "Config.hpp"
 #include "Fred.hpp"
 
+unsigned Game::getEventOfKey(SDL_Keycode keycode)
+{
+    switch (keycode)
+    {
+    case SDLK_UP:
+        return EVENT_UP;
+    case SDLK_DOWN:
+        return EVENT_DOWN;
+    case SDLK_LEFT:
+        return EVENT_LEFT;
+    case SDLK_RIGHT:
+        return EVENT_RIGHT;
+    case SDLK_SPACE:
+        return EVENT_FIRE;
+    default:
+        return 0;
+    }
+}
+
 Game::Game(Config const &cfg, std::minstd_rand &random_engine,
     TextureManager const &tmgr)
     : tmgr(tmgr)
@@ -16,4 +35,18 @@ void Game::renderSprites(SDL_Renderer *renderer) const
         for (auto const &s: sprites)
             s->render(frame, tmgr, renderer);
     }
+}
+
+void Game::moveFrame(int deltax, int deltay)
+{
+
+    frame.moveFrame(deltax, deltay);
+    if (deltax > 0)
+        game_map.updateMapBlocksRight(tmgr, frame, getSpriteList(SpriteClass::BLOCK));
+    else if (deltax < 0)
+        game_map.updateMapBlocksLeft(tmgr, frame, getSpriteList(SpriteClass::BLOCK));
+    if (deltay > 0)
+        game_map.updateMapBlocksDown(tmgr, frame, getSpriteList(SpriteClass::BLOCK));
+    else if (deltay < 0)
+        game_map.updateMapBlocksUp(tmgr, frame, getSpriteList(SpriteClass::BLOCK));
 }
