c4010ac822678bec179c1c37b21f527a3d3ac30c
Miguel Catalina-Gallego <miguel.catalina@gmail.com>
2023-09-10 09:58:19 -0400

    Revert "Use states in acid drops."
    The latest scratch implementation and the latest version of the state
    diagram reverts to using the frame id and the position of the drop to
    determine the next state.

--- AcidDrop.hpp
+++ AcidDrop.hpp
@@ -4,16 +4,7 @@
 
 class AcidDrop : public Sprite
 {
-    enum State
-    {
-        GROW,
-        FALL,
-        SPLASH,
-        RESET,
-    };
-    State state = State::GROW;
     unsigned frame_id = 0;
-
 public:
     AcidDrop(MapPos const &pos, unsigned frame_id)
     : Sprite::Sprite(pos), frame_id(frame_id) {}
