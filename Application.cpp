4e87d923936b563be9fb1aa7cbd461edb1157029
Miguel Catalina-Gallego <miguel.catalina@gmail.com>
2023-09-24 17:27:46 -0400

    Adjust end-of-level sequence.
    Per the original game the sequence should be: Fred jumps on the exit
    rope, collision detecion takes place, then at the begining of the next
    iteration (in the CLIMB state) the exit condition is checked, and the
    end-of-level sequence is started.

--- Application.cpp
+++ Application.cpp
@@ -89,7 +89,14 @@ FredApp::LevelStatus FredApp::playLevel(Game &game)
         if ((events & Game::EVENT_SHIFT) != 0 && cfg.debug_keys)
             debugMode(game, events);
         else
+        {
             fred->updateFred(game, events);
+            if (fred->exiting())
+            {
+                endOfLevelSequence(game);
+                return LevelStatus::NEXT_LEVEL;
+            }
+        }
 
         checkBulletCollisions(game);
         checkCollisionsWithEnemies(game);
@@ -97,11 +104,6 @@ FredApp::LevelStatus FredApp::playLevel(Game &game)
         {
             gameOverSequence(game);
             return LevelStatus::GAME_OVER;
-        }
-        else if (fred->exiting())
-        {
-            endOfLevelSequence(game);
-            return LevelStatus::NEXT_LEVEL;
         }
         fred->checkCollisionWithObject(game);
 
@@ -428,12 +430,12 @@ void FredApp::endOfLevelSequence(Game &game)
          i <= static_cast<int>(SpriteClass::GHOST); ++i)
         game.getSpriteList(static_cast<SpriteClass>(i)).clear();
     game.render(getRenderer());
-    SDL_Delay(cfg.ticks_per_frame / 2);
+    SDL_Delay(cfg.ticks_per_frame);
     for (int i = 0; i < 4; ++i)
     {
         fred->updateFred(game, 0);
         game.render(getRenderer());
-        SDL_Delay(cfg.ticks_per_frame / 2);
+        SDL_Delay(cfg.ticks_per_frame);
     }
 }
 
