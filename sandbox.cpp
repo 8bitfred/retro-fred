b9ce8bb45cf44829b769c8538cea020d00e0c9e4
Miguel Catalina-Gallego <miguel.catalina@gmail.com>
2023-10-26 22:35:59 -0400

    Initial implementation of signal/slot mechanism.

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,7 +1,38 @@
 #include "sdl.hpp"
+#include "Signal.hpp"
 #include <iostream>
 
-int main(int, char *[])
+
+void test_signals()
+{
+    Signal<void, int> sigtest;
+
+    std::vector<Connection> vec;
+    vec.reserve(10);
+
+    vec.emplace_back(sigtest.connect([&vec](int x)
+                                     {
+                                        std::cout << "fn0: " << x << std::endl;
+                                        if (vec.size() > 1)
+                                            vec.erase(vec.begin());
+                                     }));
+    vec.emplace_back(sigtest.connect([](int x)
+                                     { std::cout << "fn1: " << x << std::endl; }));
+    vec.emplace_back(sigtest.connect([](int x)
+                                     { std::cout << "fn2: " << x << std::endl; }));
+    sigtest(5);
+    // if (vec.size() > 1)
+    //     vec.erase(vec.begin() + 1);
+
+    //vec.pop_back();
+    //vec.erase(vec.begin());
+    std::cout << "vec.size=" << vec.size() << std::endl;
+    sigtest(6);
+    vec.front().disconnect();
+    sigtest(7);
+}
+
+int test_events()
 {
     using namespace std::string_view_literals;
     sdl::App test_app;
@@ -26,4 +57,11 @@ int main(int, char *[])
     }
     std::cout << "event error: " << SDL_GetError() << std::endl;
     return 1;
+}
+
+int main()
+{
+    test_signals();
+
+    return 0;
 }
