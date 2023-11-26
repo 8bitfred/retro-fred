df2d96fc0ecee4cb9424c2d0b1b1bf557fb2aee2
Miguel Catalina-Gallego <miguel.catalina@gmail.com>
2023-11-26 17:57:18 -0500

    Follow scratch code for object distribution algorithm.
    Adjust incorrect object flags for level 4.

--- Object.cpp
+++ Object.cpp
@@ -9,7 +9,7 @@ void Object::initialize(std::minstd_rand &random_engine, Game &game)
     std::uniform_int_distribution<> distrib_t(0, static_cast<int>(Object::Type::COUNT));
     // TODO: the number of objects depends on the game level. Also not all objects are
     // allowed in all levels.
-    for (int counter = 0; counter < game.getSpriteCount().objects;)
+    for (int counter = game.getSpriteCount().objects; counter > 0;)
     {
         MapPos pos = {distrib_x(random_engine), distrib_y(random_engine), 1, 3};
         if (game.getGameMap().getBlock(pos.cellPos()) != GameMap::Cell::EMPTY)
@@ -17,8 +17,8 @@ void Object::initialize(std::minstd_rand &random_engine, Game &game)
         Object::Type object_type = {};
         if (counter >= 16)
             object_type = Object::Type::BULLETS;
-        else if (counter < static_cast<int>(Object::Type::COUNT))
-            object_type = static_cast<Object::Type>(counter);
+        else if (counter <= static_cast<int>(Object::Type::COUNT))
+            object_type = static_cast<Object::Type>(counter-1);
         else
             object_type = static_cast<Object::Type>(distrib_t(random_engine));
         while ((!game.getSpriteCount().has_busts && object_type == Object::Type::BUST) ||
@@ -28,7 +28,7 @@ void Object::initialize(std::minstd_rand &random_engine, Game &game)
             object_type = static_cast<Object::Type>(distrib_t(random_engine));
         }
         sprite_list.emplace_back(std::make_unique<Object>(pos, object_type));
-        ++counter;
+        --counter;
     }
 }
 
