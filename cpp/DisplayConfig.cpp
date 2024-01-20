6b9f89d1b2e1b5fc4bbd8591890d37da4263029e
Miguel Catalina-Gallego <miguel.catalina@gmail.com>
2024-01-20 15:53:17 -0500

    Improve viewport scaling for full-map mode
    In full map mode, ensure that the splash screen, menu screen, today's
    greatests and level transition screens are displayed at a reasonable
    size. With the new setup the game window is centered in the system
    window, and the other screens (splash, menu, tg, transition) are
    always centered in the game window.

--- cpp/DisplayConfig.cpp
+++ cpp/DisplayConfig.cpp
@@ -5,8 +5,8 @@ DisplayConfig::DisplayConfig(Config const &cfg,
                              SDL_Window *window, SDL_Renderer *renderer) noexcept
     : window(window)
     , renderer(renderer)
-    , logical_width(cfg.logical_width)
-    , logical_height(cfg.logical_height)
+    , intro_window_w(cfg.logical_width)
+    , intro_window_h(cfg.logical_height)
     , max_resolution(cfg.max_resolution)
 {
     int window_w, window_h;
@@ -16,13 +16,13 @@ DisplayConfig::DisplayConfig(Config const &cfg,
     {
         auto max_w = (cfg.map_width * MapPos::CELL_WIDTH + 20) * MapPos::PIXELS_PER_CHAR;
         auto max_h = (cfg.map_height * MapPos::CELL_HEIGHT + 15) * MapPos::PIXELS_PER_CHAR;
-        game_window_width = std::min(max_w, window_w);
-        game_window_height = std::min(max_h, window_h);
+        game_window_w = std::min(max_w, window_w);
+        game_window_h = std::min(max_h, window_h);
     }
     else
     {
-        game_window_width = cfg.logical_width;
-        game_window_height = cfg.logical_height;
+        game_window_w = cfg.logical_width;
+        game_window_h = cfg.logical_height;
     }
 }
 
@@ -31,25 +31,33 @@ void DisplayConfig::setIntroViewport() const
     int window_w, window_h;
     SDL_GetWindowSize(window, &window_w, &window_h);
 
-    double scale_w = static_cast<double>(window_w) / logical_width;
-    double scale_h = static_cast<double>(window_h) / logical_height;
-    auto scale = std::min(scale_w, scale_h);
-    SDL_Rect rect = {(static_cast<int>(window_w / scale) - logical_width) / 2,
-                     (static_cast<int>(window_h / scale) - logical_height) / 2,
-                     logical_width,
-                     logical_height};
+    auto gw_scale_w = static_cast<double>(window_w) / game_window_w;
+    auto gw_scale_h = static_cast<double>(window_h) / game_window_h;
+    auto gw_scale = std::min(gw_scale_w, gw_scale_h);
+    auto iw_scale_w = static_cast<double>(game_window_w) / intro_window_w;
+    auto iw_scale_h = static_cast<double>(game_window_h) / intro_window_h;
+    auto iw_scale = std::min(iw_scale_w, iw_scale_h);
+    auto scale = gw_scale * iw_scale;
 
+    SDL_Rect rect{(static_cast<int>(window_w / scale) - intro_window_w) / 2,
+                  (static_cast<int>(window_h / scale) - intro_window_h) / 2,
+                  intro_window_w, intro_window_h};
     SDL_RenderSetScale(renderer, scale, scale);
     SDL_RenderSetViewport(renderer, &rect);
 }
 
 void DisplayConfig::setGameViewport() const
 {
-    if (max_resolution)
-    {
-        SDL_RenderSetScale(renderer, 1, 1);
-        SDL_RenderSetViewport(renderer, nullptr);
-    }
-    else
-        setIntroViewport();
+    int window_w, window_h;
+    SDL_GetWindowSize(window, &window_w, &window_h);
+
+    auto scale_w = static_cast<double>(window_w) / game_window_w;
+    auto scale_h = static_cast<double>(window_h) / game_window_h;
+    auto scale = std::min(scale_w, scale_h);
+
+    SDL_Rect rect{(static_cast<int>(window_w / scale) - game_window_w) / 2,
+                  (static_cast<int>(window_h / scale) - game_window_h) / 2,
+                  game_window_w, game_window_h};
+    SDL_RenderSetScale(renderer, scale, scale);
+    SDL_RenderSetViewport(renderer, &rect);
 }
--- cpp/DisplayConfig.hpp
+++ cpp/DisplayConfig.hpp
@@ -7,14 +7,14 @@ class DisplayConfig
 {
     SDL_Window *window;
     SDL_Renderer *renderer;
-    int game_window_width = 0, game_window_height = 0;
-    int logical_width = 0, logical_height = 0;
+    int game_window_w = 0, game_window_h = 0;
+    int intro_window_w = 0, intro_window_h = 0;
     bool max_resolution;
 
 public:
     DisplayConfig(Config const &cfg, SDL_Window *window, SDL_Renderer *renderer) noexcept;
-    int getGameWindowWidth() const { return game_window_width; }
-    int getGameWindowHeight() const { return game_window_height; }
+    int getGameWindowWidth() const { return game_window_w; }
+    int getGameWindowHeight() const { return game_window_h; }
     void setIntroViewport() const;
     void setGameViewport() const;
 };
