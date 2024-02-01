74e236a9e8e4ced637f12382d7bee7812a59480d
Miguel Catalina-Gallego <miguel.catalina@gmail.com>
2024-01-31 23:17:47 -0500

    Use callback (pull method) to supply audio data to driver
    Set up a callback to get the audio data into the audio device. Use our
    own circular buffer instead of the internal SDL audio queue.
    
    This solution is not complete. It's missing support for clearing
    queues.
    
    Using this mechanism was useful to determine how frequently the
    callback gets called. In Android it was determined that the callback
    gets called every 40ms. This is quite significant, and produces a
    noticeable lag in some sounds. Depending on this frequency it may be
    necessary to sync the game loop and video output with the audio
    callback.

--- cpp/SoundManager.cpp
+++ cpp/SoundManager.cpp
@@ -1,6 +1,94 @@
 #include "SoundManager.hpp"
 #include "Config.hpp"
 
+bool AudioCallbackData::add(SoundID element)
+{
+    SDL_LockMutex(mutex);
+    auto next = (tail + 1) % SIZE;
+    bool avail = next != head;
+    if (avail)
+    {
+        data[tail] = SoundRequest{element, SDL_GetTicks()};
+        tail = next;
+    }
+    SDL_UnlockMutex(mutex);
+    return avail;
+}
+
+std::optional<SoundID> AudioCallbackData::get()
+{
+    static Uint32 max_delay = 0;
+    std::optional<SoundID> result;
+    SDL_LockMutex(mutex);
+    if (head != tail)
+    {
+        result = data[head].sound_id;
+        auto delay = SDL_GetTicks() - data[head].time;
+        max_delay = std::max(max_delay, delay);
+        SDL_Log("delay=%u max=%u", delay, max_delay);
+        head = (head + 1) % SIZE;
+    }
+    SDL_UnlockMutex(mutex);
+    return result;
+}
+
+void AudioCallbackData::getAudioData()
+{
+    auto sound_id = get();
+    if (sound_id)
+    {
+        auto const &wav_data = smgr.get(*sound_id);
+        audio_data = wav_data.getBuf();
+        audio_len = wav_data.getLen();
+    }
+}
+
+void AudioCallbackData::clearQueue()
+{
+    // TODO: we need to lock the mutex before we clear the queue, and set audio_buf and
+    // audio_len. We also need to expand the code covered by the lock in the callback
+    // function
+}
+
+
+void AudioCallbackData::audio_callback(void *userdata, Uint8 *stream, int stream_len)
+{
+    auto cd = reinterpret_cast<AudioCallbackData *>(userdata);
+    auto now = SDL_GetTicks();
+    //SDL_Log("delta_t=%u", now - cd->timestamp);
+    cd->timestamp = now;
+    while (stream_len > 0)
+    {
+        if (!cd->audio_data || cd->audio_len == 0)
+        {
+            cd->getAudioData();
+            if (!cd->audio_data)
+            {
+                SDL_memset(stream, cd->device_spec.silence, stream_len);
+                stream += stream_len;
+                stream_len -= stream_len;
+                break;
+            }
+            //SDL_Log("delta_t=%u", now - cd->timestamp);
+            cd->timestamp = now;
+        }
+        auto remaining = cd->audio_len - cd->audio_pos;
+        auto len = std::min(static_cast<Uint32>(stream_len), remaining);
+        SDL_memcpy(stream, cd->audio_data + cd->audio_pos, len);
+        //SDL_Log("audio_len=%u audio_pos=%u stream_len=%d len=%u",
+        //        cd->audio_len, cd->audio_pos, stream_len, len);
+        stream += len;
+        stream_len -= len;
+        cd->audio_pos += len;
+        if (cd->audio_pos >= cd->audio_len)
+        {
+            cd->audio_data = nullptr;
+            cd->audio_len = 0;
+            cd->audio_pos = 0;
+        }
+    }
+}
+
 std::vector<sdl::WAVData> SoundManager::loadWAVs(Config const &cfg)
 {
     static char const *wav_files[] = {
@@ -28,23 +116,30 @@ std::vector<sdl::WAVData> SoundManager::loadWAVs(Config const &cfg)
     return wav_list;
 }
 
-SDL_AudioSpec SoundManager::initAudioSpec(sdl::WAVData const &wav_data)
+sdl::AudioDevice SoundManager::initAudioDevice(sdl::WAVData const &wav_data,
+                                               AudioCallbackData *callback_data)
 {
     SDL_AudioSpec audio_spec = *wav_data.getSpec();
-    audio_spec.samples = 256;
-    return audio_spec;
+    audio_spec.samples = 64;
+    audio_spec.callback = AudioCallbackData::audio_callback;
+    audio_spec.userdata = callback_data;
+    return sdl::AudioDevice(nullptr, false, &audio_spec,
+                            callback_data->getDeviceSpec(), 0);
 }
 
 SoundManager::SoundManager(Config const &cfg)
-    : wav_list(loadWAVs(cfg)), audio_spec(initAudioSpec(wav_list.front()))
-    , audio_device(nullptr, false, &audio_spec, nullptr, 0)
+    : wav_list(loadWAVs(cfg))
+    , callback_data(*this)
+    , audio_device(initAudioDevice(wav_list.front(), &callback_data))
 {
     audio_device.pause(false);
 }
 
 void SoundManager::play(SoundID sound_id)
 {
-    audio_device.queueAudio(wav_list[static_cast<size_t>(sound_id)]);
+    SDL_Log("play: %d", static_cast<int>(sound_id));
+    callback_data.add(sound_id);
+    //audio_device.queueAudio(wav_list[static_cast<size_t>(sound_id)]);
 }
 
 sdl::WAVData const &SoundManager::get(SoundID sound_id) const
--- cpp/SoundManager.hpp
+++ cpp/SoundManager.hpp
@@ -1,23 +1,58 @@
 #pragma once
 
 #include <vector>
+#include <array>
+#include <optional>
 #include "sdl.hpp"
 #include "SoundID.hpp"
 
 struct Config;
+class SoundManager;
+
+class AudioCallbackData
+{
+    static constexpr size_t SIZE = 16;
+    SDL_mutex *mutex;
+    size_t head = 0, tail = 0;
+    struct SoundRequest {
+        SoundID sound_id;
+        Uint32 time;
+    };
+    std::array<SoundRequest, SIZE> data;
+    SoundManager const &smgr;
+    SDL_AudioSpec device_spec;
+    Uint8 const *audio_data = nullptr;
+    Uint32 audio_len = 0;
+    Uint32 audio_pos = 0;
+    Uint32 timestamp = 0;
+
+public:
+    explicit AudioCallbackData(SoundManager const &smgr)
+    : mutex(SDL_CreateMutex()), smgr(smgr) {}
+    ~AudioCallbackData() { SDL_DestroyMutex(mutex); }
+    AudioCallbackData(AudioCallbackData const &) = delete;
+    AudioCallbackData &operator=(AudioCallbackData const &) = delete;
+    SDL_AudioSpec *getDeviceSpec() { return &device_spec; }
+    bool add(SoundID element);
+    std::optional<SoundID> get();
+    void getAudioData();
+    void clearQueue();
+    static void audio_callback(void *userdata, Uint8 *stream, int stream_len);
+};
 
 class SoundManager
 {
     std::vector<sdl::WAVData> wav_list;
-    SDL_AudioSpec audio_spec;
+    AudioCallbackData callback_data;
     sdl::AudioDevice audio_device;
 
     static std::vector<sdl::WAVData> loadWAVs(Config const &cfg);
-    static SDL_AudioSpec initAudioSpec(sdl::WAVData const &wav_data);
+    static sdl::AudioDevice initAudioDevice(sdl::WAVData const &wav_data,
+                                            AudioCallbackData *callback_data);
 
 public:
     explicit SoundManager(Config const &cfg);
     void play(SoundID sound_id);
     sdl::WAVData const &get(SoundID sound_id) const;
-    void clearQueuedAudio() { audio_device.clearQueuedAudio(); }
+    void clearQueuedAudio() { callback_data.clearQueue(); }
 };
