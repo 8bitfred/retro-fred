d1c0024374929dcc8f9a6f2b767e729929975c3b
Miguel Catalina-Gallego <miguel.catalina@gmail.com>
2024-02-13 11:26:38 -0500

    Clean up auto play code
    Create a wrapper class to hold the GameRunner objects and other
    objects necessary to run a game to collect data. Add a couple of
    simple examples that show how to collect data from a single game.

--- cpp/autofred.cpp
+++ cpp/autofred.cpp
@@ -94,27 +94,123 @@ GameEvent Player::getMove()
         return GameEvent::DOWN;
 }
 
-void mainLoop(FredApp &fred_app, std::minstd_rand &random_engine)
-{
-    auto const &cfg = fred_app.getConfig();
-    EventManager event_manager(cfg.ticks_per_frame, cfg.virtual_controller);
-    Window play_window(cfg,
-                       fred_app.getDisplayConfig().getGameWindowWidth(),
-                       fred_app.getDisplayConfig().getGameWindowHeight());
-    GameRunner game(cfg, random_engine, 0);
 
-    game.initializeSprites(random_engine);
-    play_window.setWindowPos(game.getFredCellPos());
-    Player player(game);
+class GameWrapper
+{
+    Window play_window;
+    GameWindow capture_window;
+    GameRunner game;
+    sdl::SurfacePtr capture_surface;
+    sdl::RendererPtr capture_renderer;
+    TextureManager capture_tmgr;
+    LabelTable label_table;
+
+public:
+    GameWrapper(Config const &cfg, std::minstd_rand &random_engine,
+                int width = 256, int height = 192)
+        : play_window(cfg, width, height)
+        , capture_window(SDL_Rect{0, 0,
+                                  play_window.getGameWindow().getWindowRect().w,
+                                  play_window.getGameWindow().getWindowRect().h})
+        , game(cfg, random_engine, 0)
+        , capture_surface(SDL_CreateRGBSurfaceWithFormat(0,
+                                                         capture_window.getWindowRect().w,
+                                                         capture_window.getWindowRect().h,
+                                                         8, SDL_PIXELFORMAT_RGBA8888))
+        , capture_renderer(SDL_CreateSoftwareRenderer(capture_surface))
+        , capture_tmgr(cfg, capture_renderer)
+        , label_table(play_window.getGameWindow().getWindowRect())
+    {
+        game.initializeSprites(random_engine);
+        play_window.setWindowPos(game.getFredCellPos());
+    }
+
+    GameRunner const &getGame() const { return game; }
+    SDL_Surface *getCaptureSurface() const { return capture_surface; }
+    LabelTable const &getLabelTable() const { return label_table; }
+
+    void renderToMemory()
+    {
+        capture_window.setPos(play_window.getGameWindow().getPos());
+        SDL_RenderClear(capture_renderer);
+        game.render(capture_tmgr, capture_renderer, capture_window);
+        game.setLabels(label_table, play_window.getGameWindow());
+    }
+
+    void renderAndPlay(DisplayConfig const &display_cfg,
+                       TextureManager const &tmgr,
+                       SoundManager &smgr,
+                       SDL_Renderer *renderer)
+    {
+        display_cfg.setGameViewport();
+        SDL_RenderClear(renderer);
+        game.render(tmgr, renderer, play_window.getGameWindow());
+        play_window.renderFrame(game, renderer, tmgr);
+        SDL_RenderPresent(renderer);
+        game.playPendingSounds(smgr);
+
+    }
+
+    GameBase::LevelStatus update(EventMask event_mask)
+    {
+        auto level_status = game.update(event_mask);
+        play_window.setWindowPos(game.getFredCellPos());
+        return level_status;
+    }
+};
+
+void dataLoop(Config const &cfg, std::minstd_rand &random_engine)
+{
+    sdl::App fred_app();
+    GameWrapper game_wrapper(cfg, random_engine);
+    Player player(game_wrapper.getGame());
+
+    static int count = 0;
+    std::ofstream label_file("labels.txt");
 
     while (true)
     {
-        fred_app.getDisplayConfig().setGameViewport();
-        SDL_RenderClear(fred_app.getRenderer());
-        game.render(fred_app.getTextureManager(), fred_app.getRenderer(), play_window.getGameWindow());
-        play_window.renderFrame(game, fred_app.getRenderer(), fred_app.getTextureManager());
-        SDL_RenderPresent(fred_app.getRenderer());
-        game.playPendingSounds(fred_app.getSoundManager());
+        game_wrapper.renderToMemory();
+
+        char file_name[200];
+        ++count;
+        std::snprintf(file_name, sizeof(file_name), "image_%04d.png", count);
+        IMG_SavePNG(game_wrapper.getCaptureSurface(), file_name);
+        label_file << file_name << "\n"
+                   << game_wrapper.getLabelTable().toString() << "\n\n\n";
+
+        EventMask event_mask;
+        event_mask.set(player.getMove());
+        if (game_wrapper.update(event_mask) != GameBase::LevelStatus::PLAY)
+            break;
+    }
+}
+
+
+void gameLoop(Config const &cfg, std::minstd_rand &random_engine)
+{
+    FredApp fred_app(cfg, random_engine);
+    GameWrapper game_wrapper(cfg, random_engine);
+    Player player(game_wrapper.getGame());
+
+    static int count = 0;
+    std::ofstream label_file("labels.txt");
+
+    EventManager event_manager(cfg.ticks_per_frame, cfg.virtual_controller);
+    while (true)
+    {
+        game_wrapper.renderAndPlay(fred_app.getDisplayConfig(),
+                                   fred_app.getTextureManager(),
+                                   fred_app.getSoundManager(),
+                                   fred_app.getRenderer());
+        game_wrapper.renderToMemory();
+
+        char file_name[200];
+        ++count;
+        std::snprintf(file_name, sizeof(file_name), "image_%04d.png", count);
+        IMG_SavePNG(game_wrapper.getCaptureSurface(), file_name);
+        label_file << file_name << "\n"
+                   << game_wrapper.getLabelTable().toString() << "\n\n\n";
 
         EventMask event_mask;
         event_mask = event_manager.collectEvents(fred_app.getWindow());
@@ -122,63 +218,20 @@ void mainLoop(FredApp &fred_app, std::minstd_rand &random_engine)
             break;
         else if (event_mask.check(GameEvent::BACK))
             break;
-        event_mask = EventMask();
-        event_mask.set(player.getMove());
-        auto level_status = game.update(event_mask);
-        if (level_status != GameBase::LevelStatus::PLAY)
+
+        EventMask player_event;
+        player_event.set(player.getMove());
+        if (game_wrapper.update(player_event) != GameBase::LevelStatus::PLAY)
             break;
-        play_window.setWindowPos(game.getFredCellPos());
     }
 }
 
-
-void dataLoop(Config const &cfg, std::minstd_rand &random_engine)
-{
-    Window play_window(cfg, 256, 192);
-    GameRunner game(cfg, random_engine, 0);
-    SDL_Rect capture_rect = play_window.getGameWindow().getWindowRect();
-    capture_rect.x = 0;
-    capture_rect.y = 0;
-    sdl::SurfacePtr soft_surface(SDL_CreateRGBSurfaceWithFormat(0, capture_rect.w, capture_rect.h,
-                                                                8, SDL_PIXELFORMAT_ARGB8888));
-    sdl::RendererPtr soft_renderer(SDL_CreateSoftwareRenderer(soft_surface));
-    TextureManager soft_tmgr(cfg, soft_renderer);
-    GameWindow soft_game_window(capture_rect);
-
-    game.initializeSprites(random_engine);
-    play_window.setWindowPos(game.getFredCellPos());
-    Player player(game);
-
-    static int count = 0;
-    LabelTable label_table(play_window.getGameWindow().getWindowRect());
-    std::ofstream label_file("labels.txt");
-
-    while (true)
-    {
-        soft_game_window.setPos(play_window.getGameWindow().getPos());
-        SDL_RenderClear(soft_renderer);
-        game.render(soft_tmgr, soft_renderer, soft_game_window);
-        game.setLabels(label_table, play_window.getGameWindow());
-
-        char file_name[200];
-        ++count;
-        std::snprintf(file_name, sizeof(file_name), "image_%04d.png", count);
-        IMG_SavePNG(soft_surface, file_name);
-        label_file << file_name << "\n"
-                   << label_table.toString() << "\n\n\n";
-
-        EventMask event_mask;
-        event_mask.set(player.getMove());
-        auto level_status = game.update(event_mask);
-        if (level_status != GameBase::LevelStatus::PLAY)
-            break;
-        play_window.setWindowPos(game.getFredCellPos());
-    }
-}
 
 int main(int argc, char* argv[])
 {
-    const Config cfg(argc, argv);
+    Config cfg(argc, argv);
+    cfg.infinite_power = true;
+    cfg.infinite_ammo = true;
 
     std::minstd_rand random_engine;
     if (cfg.debug_map)
@@ -189,10 +242,9 @@ int main(int argc, char* argv[])
         std::uniform_int_distribution<std::uint32_t> distrib;
         random_engine.seed(distrib(random_dev));
     }
-    sdl::App fred_app();
-    dataLoop(cfg, random_engine);
-    // FredApp fred_app(cfg, random_engine);
-    // mainLoop(fred_app, random_engine);
-    // fred_app.dataLoop();
+    // example 1: run a game and collect frames and their labels
+    //dataLoop(cfg, random_engine);
+    // example 2: run a game, show it in a window, and collect frames and labels
+    gameLoop(cfg, random_engine);
     return 0;
 }
