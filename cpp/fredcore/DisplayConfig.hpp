d4aa409297a5d2822eb435cd79972232e7576f95
Miguel Catalina-Gallego <miguel.catalina@gmail.com>
2024-06-16 13:49:41 -0400

    Refactor DisplayConfig initialization
    Use separate functions to initialize for the full
    map or regular modes.

--- cpp/fredcore/DisplayConfig.hpp
+++ cpp/fredcore/DisplayConfig.hpp
@@ -1,23 +1,25 @@
 #pragma once
 
 #include "sdl.hpp"
+#include <optional>
 
 struct Config;
 
 class DisplayConfig
 {
-    std::pair<sdl::WindowPtr, sdl::RendererPtr> w_and_r;
+    std::optional<sdl::WindowPtr> window;
+    std::optional<sdl::RendererPtr> renderer;
     int game_window_w = 0, game_window_h = 0;
     int intro_window_w = 0, intro_window_h = 0;
-    bool max_resolution;
 
-    static std::pair<int, int> fullMapModeSize(Config const &cfg);
-    static std::pair<sdl::WindowPtr, sdl::RendererPtr> initDisplay(Config const &cfg);
+    void initWindowAndRenderer(int width, int height, Uint32 window_flags);
+    void initFullMapMode(Config const &cfg, SDL_DisplayMode const &display_mode);
+    void initNormalMode(Config const &cfg, SDL_DisplayMode const &display_mode);
 
 public:
     explicit DisplayConfig(Config const &cfg) noexcept;
-    SDL_Window *getWindow() const { return w_and_r.first; }
-    SDL_Renderer *getRenderer() const { return w_and_r.second; }
+    SDL_Window *getWindow() const { return *window; }
+    SDL_Renderer *getRenderer() const { return *renderer; }
     int getGameWindowWidth() const { return game_window_w; }
     int getGameWindowHeight() const { return game_window_h; }
     void setIntroViewport() const;
